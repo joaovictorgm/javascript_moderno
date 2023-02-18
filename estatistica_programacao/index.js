518a3fef5dd415fd498ba3ea1c821810941aa9be
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2023-02-18 11:41:56 -0300

    adicionando a função para calcular a moda

--- estatistica_programacao/index.js
+++ estatistica_programacao/index.js
@@ -43,8 +43,27 @@ console.log(median(8, 3 , 5 , 1, 3))
 //Versão do professor 
 
 const median1 = (...numbers) => {
-    const orderedNumbers =  
+    const orderedNumbers =  [...numbers].sort((a,b) => a - b)
+    const middle = Math.floor(orderedNumbers.length / 2 )
+    if (orderedNumbers % 2 !== 0) {
+        return orderedNumbers[middle]
+    }
+
+    const firstMedian = orderedNumbers [middle - 1]
+    const secondMedian = orderedNumbers[middle]
+    return average (firstMedian, secondMedian)
 }
 
+console.log(`Mediana: ${median(2, 5, 99, 4, 42, 7)}`)
+console.log(`Mediana: ${median(15, 14, 8, 7, 3)}`)
 
+const mode = (...numbers) => {
+    const quantities = numbers.map(num => [
+        num,
+        numbers.filter(n => num === n).length
+    ])
+    quantities.sort((a, b) => b[1] - a[1])
+    return quantities[0][0]
+}
 
+console.log(`Moda: ${mode(1, 1, 99, 5, 4, 9, 4, 2,4 )}`)
