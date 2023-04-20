0c44db132fbbdc0f3d10df50b57d67268c988997
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2023-04-20 10:35:18 -0300

    usando babel e npm em um projeto base

--- webpack_ex011/src/index.js
+++ webpack_ex011/src/index.js
@@ -1,3 +1,6 @@
-const dayjs = require('dayjs')
+import dayjs from 'dayjs'
+import './styles/index.css'
 
 alert(`Hoje é: ${dayjs().format("DD/MM/YYYY")}`)
+
+//importando os loaders 
---
+++ webpack_ex011/webpack.config.js
@@ -0,0 +1,16 @@
+
+
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    mode: 'development',
+   module: {
+    rules :[{
+        test:/\.css$/,
+        use: ['style-loader', 'css-loader']
+    }]
+   }
+
+}
